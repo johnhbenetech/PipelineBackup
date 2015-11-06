535a5167ce96e8fc226ca8c952e96454a52bbb35
iheartqwerty <iheartqwerty@gmail.com>
2015-11-06 10:09:26 -0800

    new

--- packages/telescope-posts/lib/client/templates/modules/post_rank.js
+++ packages/telescope-posts/lib/client/templates/modules/post_rank.js
@@ -1,9 +1,9 @@
-Template.post_rank.helpers(
-  {
+Template.post_rank.helpers({
   oneBasedRank: function(){
     if (typeof this.rank !== 'undefined') {
-      return this.rank + 1;}
-    },    
+      return this.rank + 1;
+    }},
+
   pipelineScore: function(){
     return this.impact+this.need+this.missionfit+this.legality+this.currentmoney+this.futuremoney+this.cost+this.talent+this.technicalrisk+this.internalchampion+this.communityofusers+this.communityofpartners+this.portfoliofit+this.exitstrategies+this.existingdata;}
 });
